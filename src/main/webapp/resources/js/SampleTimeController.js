af59832fa82fe3b6d54482f54585adf3fbcc6962
Ravi <Ravi@RAVIDESKTOP-8VS21QU>
2017-04-13 10:33:53 +0530

    controller

--- src/main/webapp/resources/js/SampleTimeController.js
+++ src/main/webapp/resources/js/SampleTimeController.js
@@ -1,6 +1,10 @@
 var app = angular.module('myApp', [ 'ui.bootstrap' ]);
 
 app.controller('UserCtrl', function($scope, $http, $uibModal) {
+	/*
+	 * The $http.get method is used to retrieve information from the given
+	   server using a given URI. 
+	 */
 	$http.get('list4?').then(function(response) {
 		$scope.names = response.data;
 	});
